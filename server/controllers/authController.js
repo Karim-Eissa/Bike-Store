04a2953fda4f4b006e8cb976db21963a9c2a5089
Karim Eissa <karim.eissa@robustastudio.com>
2024-11-11 19:30:43 +0200

    initial backend commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken')
+module.exports = {
+	loginUser_post : async (req, res) => {
+			
+	},
+	signupUser_post: async (req, res) => {
+	
+	},
+	logoutUser_post: async (req, res) => {
+	
+	},
+}
+
---
+++ server/routes/appRoutes.js
@@ -0,0 +1,11 @@
+const router=express.Router();
+const appController=require('../controllers/appController')
+router.get('', );
+router.get('', );
+router.get('', );
+router.get('', );
+router.get('', );
+router.get('', );
+router.get('', );
+router.get('', );
+module.exports = router;
